855efa29c907eecb37feca1caa74abaeabfd5868
Onkar Shinde <onkarshinde499@gmail.com>
2026-01-09 13:22:24 +0530

    Work section done

--- app/component/Work.jsx
+++ app/component/Work.jsx
@@ -46,6 +46,10 @@ const Work = () => {
           </div>
         ))}
       </div>
+      <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[.5px]
+      border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lighthover duration-500"> 
+        Show more <Image src={assets.right_arrow_bold} alt="" className="w-4"/>
+      </a>
     </div>
   );
 };
